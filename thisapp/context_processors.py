4906192c4ad722d000e0a4b972414304ef292e77
Vijay Thapa <vijaythapa333@gmail.com>
2024-12-29 15:43:52 +0545

    Displaying Website Details on Footer

--- thisapp/context_processors.py
+++ thisapp/context_processors.py
@@ -4,10 +4,11 @@ from .models import AppDetail, SocialMedia
 
 def app_detail(request):
 	# this_app = AppDetail.objects.filter(id__lookup=1).latest()
+	this_app = AppDetail.objects.get(app_name='Travels')
 	socialmedias = SocialMedia.objects.filter(socialmedia_is_active='yes')
 
 	context = {
-		# 'this_app': this_app,
+		'this_app': this_app,
 		'socialmedias': socialmedias
 	}
 	return context
